86cfe4897e80c560886be4b941afbf091b19f652
Patafix <project.patafix@gmail.com>
2023-07-20 02:09:02 +0200

    Make Art not War

--- src/player.js
+++ src/player.js
@@ -1,6 +1,9 @@
-let auth_token = undefined;
+let setToken;
+
+let access_token = new Promise((resolve) => (setToken = resolve));
 
 let refresh_token = localStorage.getItem("refresh_token");
+
 if (refresh_token !== null) {
     fetch(`/refresh-token?refresh_token=${refresh_token}`)
         .then(async (res) => {
@@ -8,8 +11,8 @@ if (refresh_token !== null) {
             console.log(res);
             if (res.refresh_token !== undefined) {
                 localStorage.setItem("refresh_token", res.refresh_token);
-                auth_token = res.auth_token;
             }
+            setToken(res.access_token);
         })
         .catch((err) => {
             console.error(err);
@@ -22,11 +25,82 @@ if (refresh_token !== null) {
     )
         .then(async (res) => {
             res = await res.json();
-            console.log(res);
             localStorage.setItem("refresh_token", res.refresh_token);
-            auth_token = res.auth_token;
+            setToken(res.access_token);
         })
         .catch((err) => {
             console.error(err);
         });
 }
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    access_token.then((access_token) => {
+        console.log(access_token);
+
+        const player = new window.Spotify.Player({
+            name: "BeamRadio",
+            getOAuthToken: (cb) => {
+                cb(access_token);
+            },
+            volume: 0.5,
+        });
+
+        player.addListener("ready", ({ device_id }) => {
+            console.log("Ready with Device ID", device_id);
+        });
+
+        player.addListener("not_ready", ({ device_id }) => {
+            console.log("Device ID has gone offline", device_id);
+        });
+
+        player.addListener("initialization_error", ({ message }) => {
+            console.error(message);
+        });
+
+        player.addListener("authentication_error", ({ message }) => {
+            console.error(message);
+        });
+
+        player.addListener("account_error", ({ message }) => {
+            console.error(message);
+        });
+
+        player.on("playback_error", ({ message }) => {
+            console.error("Failed to perform playback", message);
+        });
+
+        player.resume().then(() => {
+            console.log("Resumed!");
+        });
+
+        document.getElementById("togglePlay").onclick = function () {
+            player.togglePlay();
+        };
+
+        document.getElementById("test").addEventListener("click", () => {
+            // The player is activated. The player will keep the
+            // playing state once the state is transferred from other
+            // applications.
+            player.activateElement();
+        });
+        // Transfer your currently playing track into your
+        // application through device picker in Spotify APP.
+
+        player.addListener(
+            "player_state_changed",
+            ({ position, duration, track_window: { current_track } }) => {
+                console.log("Currently Playing", current_track);
+                console.log("Position in Song", position);
+                console.log("Duration of Song", duration);
+            }
+        );
+
+        player.addListener("autoplay_failed", () => {
+            console.log(
+                "Autoplay is not allowed by the browser autoplay rules"
+            );
+        });
+
+        player.connect();
+    });
+};
--- src/server.js
+++ src/server.js
@@ -5,12 +5,36 @@ const axios = require("axios");
 const { access } = require("node:fs");
 require("dotenv").config();
 
+const scope =  "streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state user-read-currently-playing";
+
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));        
+    }
+    return text;
+}
+
 app.get("/", (req, res) => {
-    res.send(
-        "<a href='https://accounts.spotify.com/authorize?client_id=" +
-            process.env.CLIENT_ID +
-            "&response_type=code&redirect_uri=http%3A%2F%2F176.151.209.242%3A7613%2Fconnected&scope=user-top-read'>Sign in</a>"
-    );
+    let state = generateRandomString(16);
+
+    let requestParameters = new URLSearchParams({
+        response_type: "code",
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: "http://localhost:7613/connected",
+        scope: scope,
+        state: state
+    });
+
+    res.send(`<a href=https://accounts.spotify.com/authorize/?${requestParameters.toString()}>Sign in</a>`)
+
+    // res.send(
+    //     "<a href='https://accounts.spotify.com/authorize?client_id=" +
+    //         process.env.CLIENT_ID +
+    //         "&response_type=code&redirect_uri=http%3A%2F%2F176.151.209.242%3A7613%2Fconnected&scope=user-top-read'>Sign in</a>"
+    // );
 });
 
 app.get("/connected", async (req, res) => {
@@ -69,7 +93,7 @@ app.use("/refresh-token", async (req, res) => {
         .then((spotifyResponse) => {
             res.send(JSON.stringify({
                 refresh_token: spotifyResponse.data.refresh_token,
-                auth_token: spotifyResponse.data.auth_token
+                access_token: spotifyResponse.data.access_token
             }));
 
             console.log(spotifyResponse.data);
